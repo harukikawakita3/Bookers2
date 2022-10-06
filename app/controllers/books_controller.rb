32f3969dad8fb2f6820a5d00834e01609912f4c4
harukikawakita3 <113086279+harukikawakita3@users.noreply.github.com>
2022-10-06 12:32:55 +0000

    finish except edit_page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,14 +15,14 @@ class BooksController < ApplicationController
 
   def index
     @books=Book.all
-    
-    
-    
+    @user=current_user
+    @book=Book.new
   end
 
   def show
     @book=Book.find(params[:id])
     @user=@book.user
+    @book_new=Book.new
   end
   
   def edit
@@ -32,14 +32,14 @@ class BooksController < ApplicationController
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
-    redirect_to "/books"
+    redirect_to books_path
   end
     
   
   private
   
   def book_params
-    params.require(:book).permit(:title, :body,)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   
   def index
+    @user=current_user
+    @users=User.all
   end
   
   def show
     @user=User.find(params[:id])
     @book=Book.new
+    @books=Book.all
+    @book_show=Book.find(params[:id])
     
     
   end
